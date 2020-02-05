d68ff27ffb00d1c7d68c50ea6f6e6a6dc1026c94
es <StaNC2000>
2020-02-05 13:38:30 +0100

    methods getHeight and getVolume created

--- src/com/eS/Cuboid.java
+++ src/com/eS/Cuboid.java
@@ -4,6 +4,7 @@ public class Cuboid extends Rectangle{
     private double height;
 
     public Cuboid(double width, double length, double height) {
+        // in case when height parameter is less then 0 set fields to 0;
         super(width, length);
         if (height < 0) {
             this.height = 0;
@@ -13,4 +14,11 @@ public class Cuboid extends Rectangle{
         }
     }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
 }
