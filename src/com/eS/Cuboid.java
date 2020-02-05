0d2c8e43b3c9ad1193aa20890eccdc367ed5dafb
es <StaNC2000>
2020-02-05 13:36:21 +0100

    Constructor created, also in case when height parameter is less then 0 set his field to 0

---
+++ src/com/eS/Cuboid.java
@@ -0,0 +1,16 @@
+package com.eS;
+
+public class Cuboid extends Rectangle{
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            this.height = 0;
+        }
+        else {
+            this.height = height;
+        }
+    }
+
+}
