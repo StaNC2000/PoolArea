513e7cb0d02e184b7843e6cd69b67f5334aacc92
es <StaNC2000>
2020-02-05 13:26:48 +0100

    in case when parameters are less then 0 individual field sets to 0

--- src/com/eS/Rectangle.java
+++ src/com/eS/Rectangle.java
@@ -6,6 +6,19 @@ public class Rectangle {
     private double length;
 
     public Rectangle(double width, double length) {
+        // in case when parameters are less then 0 set fields to 0;
 
+        if (width < 0) {
+            this.width = 0;
+        }
+        else {
+            this.width = width;
+        }
+        if (length < 0) {
+            this.length = 0;
+        }
+        else {
+            this.length = length;
+        }
     }
 }
